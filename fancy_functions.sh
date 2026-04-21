319c30f488d13ea7ca63393c51f05c654afffe9f
DeveshKB <dkb020102@gmail.com>
2026-04-21 11:42:32 +0400

    fancy_functions update

--- fancy_functions.sh
+++ fancy_functions.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
 gco() {
-    # Vérifie si on est dans un repo git
     if ! git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
-        echo "Erreur : ce n'est pas un dépôt Git."
+        echo "Erreur : pas un repo Git"
         return 1
     fi
 
-    # Vérifie si un message est fourni
     if [ -z "$1" ]; then
-        echo "Usage : gco 'message de commit'"
+        echo "Usage : gco 'message'"
         return 1
     fi
 
-    # Récupère le nom de la branche
     branch=$(git branch --show-current)
-
-    # Commit formaté
     git commit -m "[$branch] $1"
 }
